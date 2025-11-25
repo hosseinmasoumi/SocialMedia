d11475464aaf5a8c94a3d08552685b33ac26ce7d
Hossein <hosseinmasoumi@users.noreply.github.com>
2025-11-25 14:33:14 +0330

    refactor: Make SingleStoryView fill the entire screen
    The `SingeStoryView` composable is updated to fill the maximum available size. The previously fixed height and width modifiers have been removed, allowing the story view to occupy the full screen.

--- app/src/main/java/com/example/test/screen/SingeStoryView.kt
+++ app/src/main/java/com/example/test/screen/SingeStoryView.kt
@@ -2,13 +2,10 @@ package com.example.test.screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil3.compose.AsyncImage
 import com.example.test.R
@@ -19,9 +16,7 @@ fun SingeStoryView(id: Int, navController: NavHostController) {
     val story= MockDataRepository.getStory(id)
     Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(270.dp)
-    ) {
+            .fillMaxSize()) {
         AsyncImage(
             model = story?.storyImage,
             contentDescription = story?.username,
